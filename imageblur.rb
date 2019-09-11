c595d6c4e4132b500ee06529d099c9778bde907c
Joel Pankito <Joelpankito@yahoo.com>
2019-09-11 03:34:38 +0000

    Initial commit

---
+++ imageblur.rb
@@ -0,0 +1,20 @@
+class Image
+  attr_accessor :array
+
+  def initialize(array)
+    @array = array
+  end
+
+  def output_image
+  @array.each { |x| puts x.join("") }
+  end
+end 
+
+
+image = Image.new([
+  [0, 0, 0, 0],
+  [0, 1, 0, 0],
+  [0, 0, 0, 1],
+  [0, 0, 0, 0]
+])
+image.output_image 
